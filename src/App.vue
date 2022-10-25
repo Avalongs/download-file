8720d763c05e6cfb6165fe0db78dac129674bcf0
BlackKey <553180854@qq.com>
2022-10-25 17:54:38 +0800

    修改url

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup>
 import { ref } from "vue";
 import download from "./components/download.vue";
-const fileUrl = ref(
-  "http://ln-smart.wxkjtec.com/group2/M00/00/0E/rBJEi2MijM-AA0FtAAsMa7BuvtM801.mp4"
-);
+const fileUrl = ref("www.baidu.com");
 </script>
 
 <template>
